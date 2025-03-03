e1d01c4731038c3c49284603a7aebac56618a9b5
SeungHyun <qustmdgus1212@gmail.com>
2025-03-03 17:25:45 +0900

    splash 액티비티 및 xml 추가

---
+++ presentation/src/main/java/com/example/presentation/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.presentation
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            installSplashScreen()
+        }
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
