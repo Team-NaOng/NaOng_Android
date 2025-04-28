9b46e7fdb7ff0def6abab818e76cb0abde6df7fd
SeungHyun <qustmdgus1212@gmail.com>
2025-04-28 23:06:38 +0900

    CustomChip 추가

---
+++ presentation/src/main/java/com/example/presentation/chip/CustomChip.kt
@@ -0,0 +1,54 @@
+package com.example.presentation.chip
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
+import com.example.presentation.R
+
+class CustomChipView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : AppCompatTextView(context, attrs) {
+
+    var isChipSelected = false
+        set(value) {
+            field = value
+            updateBackground()
+        }
+
+    init {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            setTextAppearance(R.style.description)
+        } else {
+            @Suppress("DEPRECATION")
+            setTextAppearance(context, R.style.description)
+        }
+
+        setPadding(
+            17.dpToPx(), 8.dpToPx(),
+            17.dpToPx(), 8.dpToPx()
+        )
+        includeFontPadding = false
+        gravity = Gravity.CENTER
+
+        setOnClickListener {
+            isChipSelected = !isChipSelected
+        }
+
+        updateBackground()
+    }
+
+    private fun updateBackground() {
+        background = if (isChipSelected) {
+            ContextCompat.getDrawable(context, R.drawable.chip_selected)
+        } else {
+            ContextCompat.getDrawable(context, R.drawable.chip_unselected)
+        }
+    }
+
+    private fun Int.dpToPx(): Int {
+        return (this * context.resources.displayMetrics.density).toInt()
+    }
+}
