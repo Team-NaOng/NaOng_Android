871fd036c161f970651821838ad18171d08183b6
SeungHyun <qustmdgus1212@gmail.com>
2025-05-06 21:54:33 +0900

    dpToPxF 추가

--- presentation/src/main/java/com/example/presentation/Dimension.kt
+++ presentation/src/main/java/com/example/presentation/Dimension.kt
@@ -2,6 +2,10 @@ package com.example.presentation
 
 import android.content.Context
 
-    fun Int.dpToPx(context: Context): Int {
-        return (this * context.resources.displayMetrics.density).toInt()
-    }
+fun Int.dpToPx(context: Context): Int {
+    return (this * context.resources.displayMetrics.density).toInt()
+}
+
+fun Int.dpToPxF(context: Context): Float =
+    this * context.resources.displayMetrics.density
+
