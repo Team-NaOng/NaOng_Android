6f242a8d08c320e013becc0236f1ebe01169c088
SeungHyun <qustmdgus1212@gmail.com>
2025-05-06 17:05:41 +0900

    TodoItem 기본값 추가 및 hasTime 변수 추가

--- presentation/src/main/java/com/example/presentation/data/TodoItem.kt
+++ presentation/src/main/java/com/example/presentation/data/TodoItem.kt
@@ -2,6 +2,7 @@ package com.example.presentation.data
 
 data class TodoItem(
     val title: String,
-    val hasRepeat: Boolean,
-    var isDone: Boolean
+    val hasRepeat: Boolean = false,
+    var isDone: Boolean = false,
+    val hasTime: Boolean = true,
 )
