a6e9e588607e765ecbd0f593e5637d14c806894c
SeungHyun <qustmdgus1212@gmail.com>
2025-05-11 17:46:22 +0900

    패키지 이동

--- domain/src/main/java/com/example/domain/todo/TodoItem.kt
+++ domain/src/main/java/com/example/domain/todo/TodoItem.kt
@@ -1,8 +1,9 @@
-package com.example.presentation.data
+package com.example.domain.todo
 
 data class TodoItem(
     val title: String,
     val hasRepeat: Boolean = false,
     var isDone: Boolean = false,
     val hasTime: Boolean = true,
+    val category: String = "위치"
 )
