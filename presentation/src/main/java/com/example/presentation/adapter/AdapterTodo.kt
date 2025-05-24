1a8cd854932d079df3e9071c159af803252e6fa1
SeungHyun <qustmdgus1212@gmail.com>
2025-05-24 23:16:50 +0900

    with(binding) 적용으로 viewholder 코드 개선

--- presentation/src/main/java/com/example/presentation/adapter/AdapterTodo.kt
+++ presentation/src/main/java/com/example/presentation/adapter/AdapterTodo.kt
@@ -25,54 +25,54 @@ class AdapterTodo : RecyclerView.Adapter<AdapterTodo.TodoViewHolder>() {
         fun bind(item: TodoItem) {
             val context = binding.root.context
 
-            binding.textTitle.text = item.title
-            binding.textTime.visibility = if (item.category == "시간") View.VISIBLE else View.GONE
-            binding.textTime.text = item.time
+            with(binding) {
+                textTitle.text = item.title
+                textTime.visibility = if (item.category == "시간") View.VISIBLE else View.GONE
+                textTime.text = item.time
 
-            val iconRes = if (item.category == "시간") R.drawable.ic_time else R.drawable.ic_location
-            binding.imageIcon.setImageResource(iconRes)
+                val iconRes =
+                    if (item.category == "시간") R.drawable.ic_time else R.drawable.ic_location
+                imageIcon.setImageResource(iconRes)
 
-            val iconBgRes = when {
-                item.hasRepeat && item.isDone -> R.drawable.bg_cell_icon_primary    // 반복 + 완료 → 진한 핑크
-                item.hasRepeat && !item.isDone -> R.drawable.bg_cell_icon_secondary // 반복 + 미완료 → 연한 핑크
-                !item.hasRepeat && item.isDone -> R.drawable.bg_cell_icon_gray      // 미반복 + 완료 → 회색
-                else -> R.drawable.bg_cell_icon_secondary                           // 미반복 + 미완료 → 연한 핑크
-            }
-            binding.viewIconBackground.setBackgroundResource(iconBgRes)
-
-            val iconColor = when {
-                item.hasRepeat && item.isDone -> R.color.secondary
-                item.isDone -> R.color.gray3
-                else -> R.color.primary
-            }
-            binding.imageIcon.setColorFilter(context.getColor(iconColor))
-
-            binding.buttonRepeat.visibility = if (item.hasRepeat) View.VISIBLE else View.GONE
-
-            if (item.isDone && !item.hasRepeat) {
-                // 완료 && 반복 아님 → 색상 변경
-                binding.textTitle.setTextColor(context.getColor(R.color.gray2))
-                binding.textTime.setTextColor(context.getColor(R.color.gray2))
-            } else {
-                // 반복인 경우나 미완료인 경우 → 원래 색상 유지
-                binding.textTitle.setTextColor(context.getColor(R.color.black))
-                binding.textTime.setTextColor(context.getColor(R.color.gray3))
-            }
-
-            itemView.setOnClickListener {
-                item.isDone = !item.isDone
-
-                if (item.isDone) {
-                    val context = itemView.context
-                    val messages = listOf(
-                        context.getString(R.string.todo_complete_1),
-                        context.getString(R.string.todo_complete_2),
-                        context.getString(R.string.todo_complete_3),
-                        context.getString(R.string.todo_complete_4)
-                    )
-                    showCustomToast(itemView, messages.random())
+                val iconBgRes = when {
+                    item.hasRepeat && item.isDone -> R.drawable.bg_cell_icon_primary    // 반복 + 완료 → 진한 핑크
+                    item.hasRepeat && !item.isDone -> R.drawable.bg_cell_icon_secondary // 반복 + 미완료 → 연한 핑크
+                    !item.hasRepeat && item.isDone -> R.drawable.bg_cell_icon_gray      // 미반복 + 완료 → 회색
+                    else -> R.drawable.bg_cell_icon_secondary                           // 미반복 + 미완료 → 연한 핑크
                 }
-                notifyItemChanged(adapterPosition)
+                viewIconBackground.setBackgroundResource(iconBgRes)
+
+                val iconColor = when {
+                    item.hasRepeat && item.isDone -> R.color.secondary
+                    item.isDone -> R.color.gray3
+                    else -> R.color.primary
+                }
+                imageIcon.setColorFilter(context.getColor(iconColor))
+
+                buttonRepeat.visibility = if (item.hasRepeat) View.VISIBLE else View.GONE
+
+                if (item.isDone && !item.hasRepeat) {
+                    textTitle.setTextColor(context.getColor(R.color.gray2))
+                    textTime.setTextColor(context.getColor(R.color.gray2))
+                } else {
+                    textTitle.setTextColor(context.getColor(R.color.black))
+                    textTime.setTextColor(context.getColor(R.color.gray3))
+                }
+
+                root.setOnClickListener {
+                    item.isDone = !item.isDone
+
+                    if (item.isDone) {
+                        val messages = listOf(
+                            context.getString(R.string.todo_complete_1),
+                            context.getString(R.string.todo_complete_2),
+                            context.getString(R.string.todo_complete_3),
+                            context.getString(R.string.todo_complete_4)
+                        )
+                        showCustomToast(root, messages.random())
+                    }
+                    notifyItemChanged(adapterPosition)
+                }
             }
         }
     }
